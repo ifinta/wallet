34ddf694374fbece659d61f3c7b05cb49dde42f5
ifinta <istvan_finta@yahoo.com>
2023-05-14 10:10:43 +0200

    kis szépítgetés vertikálisan

--- src/components/OneTx.js
+++ src/components/OneTx.js
@@ -6,16 +6,15 @@ export function formatDate(dateString) {
 }
 
 export default function OneTx({id, created_at, created_by_name, title, amount, onDelete}) {
-
-    return (<Grid container spacing={2}>
+    return (<Grid container spacing={2} alignItems="center">
                 <Grid item xs={12} md={4} lg={3}>{formatDate(created_at)}</Grid>
                 <Grid item xs={12} md={2} lg={2}>{created_by_name}</Grid>
-                <Grid item xs={12} md={5} lg={6}>
+                <Grid item xs={12} md={1} lg={1} style={{ display: "block" }}>
                     {onDelete && <IconButton onClick={onDelete}>
-                        <Delete/>
+                            <Delete/>
                     </IconButton>}
-                    {title}
                 </Grid>
+                <Grid item xs={12} md={4} lg={5}>{title}</Grid>
                 <Grid item xs={12} md={1} lg={1}>{amount}</Grid>
 	    </Grid>);
 }
